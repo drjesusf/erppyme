dc1aa41ac48b3d72a05a6ec433abe14d6996442d
Jesus Ramirez <drjesusf@gmail.com>
2012-12-25 16:27:20 -0500

    Seguridad Login y URLs

--- src/main/java/org/erppyme/security/Usuario.java
+++ src/main/java/org/erppyme/security/Usuario.java
@@ -1,13 +1,20 @@
 package org.erppyme.security;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Usuario {
 	private int codUsuario;
 	private String nombres;
 	private String apellidos;
 	private String usuario;
 	private String password;
+	
+	private Set<Rol> roles;  
+	
 	public Usuario() {
 		super();
+		roles = new HashSet<Rol>(0);
 	}
 	public Usuario(int codUsuario, String nombres, String apellidos,
 			String usuario, String password) {
@@ -17,6 +24,8 @@ public class Usuario {
 		this.apellidos = apellidos;
 		this.usuario = usuario;
 		this.password = password;
+		
+		roles = new HashSet<Rol>(0);
 	}
 	public int getCodUsuario() {
 		return codUsuario;
@@ -47,6 +56,12 @@ public class Usuario {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	public Set<Rol> getRoles() {
+		return roles;
+	}
+	public void setRoles(Set<Rol> roles) {
+		this.roles = roles;
 	}
 	
 	
--- src/main/java/org/erppyme/service/CustomUserDetailsService.java
+++ src/main/java/org/erppyme/service/CustomUserDetailsService.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.List;
 
 import org.erppyme.repository.UsuarioRepository;
+import org.erppyme.security.Rol;
 import org.erppyme.security.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
@@ -22,21 +23,36 @@ public class CustomUserDetailsService implements UserDetailsService {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
-//	@Autowired
-//	public CustomUserDetailsService(UsuarioRepository usuarioRepository){
-//		this.usuarioRepository = usuarioRepository;
-//	}
+	public CustomUserDetailsService(){}
 	
 	@Override
 	public UserDetails loadUserByUsername(String username)
 			throws UsernameNotFoundException {
+		
 		Usuario usuarioExistente = usuarioRepository.obtenerUsuarioPorUsername(username);
-		return new User(usuarioExistente.getUsuario(),usuarioExistente.getPassword(),true,true,true,true,getAuthorities(1));
+		 List<Rol> listRols = new ArrayList<Rol>(usuarioExistente.getRoles());
+		User user = new User(usuarioExistente.getUsuario(),usuarioExistente.getPassword(),
+								true,true,true,true,getAuthorities(listRols));
+	
+		return user;
 	}
 	
-	public Collection<? extends GrantedAuthority> getAuthorities(Integer role) {
-		List<GrantedAuthority> authList = getGrantedAuthorities(getRoles(role));
+	public Collection<? extends GrantedAuthority> getAuthorities(List<Rol> roles) {
+		List<String> lstRoles = new ArrayList<String>();
+		for (Rol rol : roles) {
+			lstRoles.add(rol.getDescripcion());
+		}
+		List<GrantedAuthority> authList = getGrantedAuthorities(lstRoles);
 		return authList;
+		
+	}
+	
+	public static List<GrantedAuthority> getGrantedAuthorities(List<String> roles) {
+		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+		for (String role : roles) {
+			authorities.add(new SimpleGrantedAuthority(role));
+		}
+		return authorities;
 	}
 	
 	public List<String> getRoles(Integer role) {
@@ -52,13 +68,4 @@ public class CustomUserDetailsService implements UserDetailsService {
 
 		return roles;
 	}
-	
-	public static List<GrantedAuthority> getGrantedAuthorities(List<String> roles) {
-		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-		for (String role : roles) {
-			authorities.add(new SimpleGrantedAuthority(role));
-		}
-		return authorities;
-	}
-
 }
